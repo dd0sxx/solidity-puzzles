8086ab0053f1ccfdb1f74c4fcbd75b43df95c816
dd0sxx <contacttheotelonis@gmail.com>
2021-11-03 13:19:39 -0400

    optimization

---
+++ Optimization.sol
@@ -0,0 +1,117 @@
+/// SPDX-License-Identifier: UNLICENSED
+/// @title Optimization Contract
+/// @author: dd0sxx
+
+/// difficulty: 3/10
+/// optimize this contract:
+
+pragma solidity ^0.8.0;
+
+
+contract UnoptimizedContract {
+    
+    uint a;
+    int256 b;
+    uint64 c;
+    bytes32 d;
+    uint128 e;
+    uint128 f;
+    bytes32 g;
+    uint128 h;
+
+    struct person {
+        string name;
+        uint8 age;
+        string country;
+        bytes32 id;
+        bytes32[] family;
+    }
+
+    function f1 (uint x) public returns (bool) {
+        for ( uint i = 0 ; i < x ; i++) {
+            a += i; 
+        }
+        for ( int j = 0 ; j < int(x); j++) {
+            b -= j; 
+        }
+        return (a > 50 || b > 50) ? true : false;
+    }
+
+    function f2 (uint x) public returns (bool) {
+        a += x;
+        b -= int(x);
+
+        return (a > 50 || b > 50) ? true : false;
+    }
+
+    function setA (uint x) public {
+        a = x;
+    }
+
+    function setB (int x) public {
+        b = x;
+    }
+    
+    function greaterThan50 ( uint x ) public returns (bool) {
+        if (f1(x)) {
+            return true;
+        } else if (f2(x)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+/// answer:
+
+contract OptimizedContract {
+    
+    uint a;
+    int256 b;
+    uint128 f;
+    uint128 e;
+    uint128 h;
+    uint64 c;
+    bytes32 d;
+    bytes32 g;
+
+    struct person {
+        bytes32[] family;
+        string name;
+        string country;
+        bytes32 id;
+        uint8 age;
+    }
+
+    function f1 (uint x) public returns (bool) {
+        uint tempA;
+        int tempB;
+        for ( uint i = 0 ; i < x ; i++) {
+            tempA += i; 
+            tempB -= int(i); 
+        }
+        a += tempA;
+        b += tempB;
+        return (a > 50 || b > 50) ? true : false;
+    }
+
+    function f2 (uint x) public returns (bool) {
+        a += x;
+        b -= int(x);
+
+        return (a > 50 || b > 50) ? true : false;
+    }
+
+    function setA (uint x) external {
+        a = x;
+    }
+
+    function setB (int x) external {
+        b = x;
+    }
+    
+    function greaterThan50 ( uint x ) public returns (bool) {
+        return (f2(x) || f1(x)) ? true : false;
+    }
+}
--- README.md
+++ README.md
@@ -2,5 +2,9 @@
 
 Each toy-problem is given a difficulty scale between 1-10 (10 being most difficult) and should take no longer than 30 minutes to complete.
 
+
+Please author your submissions, provide answers, and always notate the version of solidity being used.
+
 The point of the toy problems is not for students to write code and get the answer right necessarily, but more to look at code and try to find the answer. Ultimately we want to expand the student's arsenal and expand their knowledge of what to look for and how to spot bugs and vulnerabilites, and optimizations.
 
+We need at minimum 17 toy problems for the 6 week flagship course at hackerDAO (6 weeks, 3 lectures per week minus 1 for the first day), but we should aim create the most comprehensive list of toy problems in the space with time.
